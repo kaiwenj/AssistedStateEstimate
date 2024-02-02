d31e995c25617615822a61a6fc2979be9859fe59
thomaslixu <100257462+thomaslixu@users.noreply.github.com>
2024-02-02 11:22:28 -0800

    Update sampling.py

--- src/sampling.py
+++ src/sampling.py
@@ -1,5 +1,9 @@
 # Sample from belief
 
+def sample_initial(init_dist):
+    # Input: 1-D array that represents probability distribution
+    # Output: Random index sampled using the probability distribution
+    return np.random.choice(np.arange(0, init_dist.size), p=init_dist)
 
 # Sample from observation
 
