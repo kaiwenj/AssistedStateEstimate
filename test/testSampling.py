b982070ade1d11b0e534ecfc53ee32dd661a719e
kaiwenj <64115446+kaiwenj@users.noreply.github.com>
2024-02-02 10:00:17 -0800

    Create testSampling.py

---
+++ test/testSampling.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.append('../src/')
+
+import unittest
+from ddt import ddt, data, unpack
+import sampling as targetCode
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
