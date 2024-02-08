d1b334104b7eb3eaf6eb2cff9ec4d70eb37e059d
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-02-08 11:47:51 -0800

    Update init_dist.py

--- src/init_dist.py
+++ src/init_dist.py
@@ -9,5 +9,17 @@ def genInitDist(labels,minlength=10):
 def genDyndDist(labels=10,actions=11):
     return np.array([np.identity(labels)]*actions)
 
+def genObsProb(dat,patterns,dataset):
+    img,label=dat
+    t_img,t_label=dataset
+    subImg=t_img[np.where(t_label == label)]
+    numImg=len(subImg)
+    patrn=img[patterns,:]
+    count=0
+    for i in range(numImg):
+        if np.array_equal(subImg[i,patterns,:],patrn):
+            count=count+1
+    return count/numImg
+
 
 
