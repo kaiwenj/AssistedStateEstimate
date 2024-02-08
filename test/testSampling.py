c1191969aaa9579d4558bf8901996b9d6750867d
thomaslixu <100257462+thomaslixu@users.noreply.github.com>
2024-02-08 11:55:17 -0800

    Update testSampling.py

--- test/testSampling.py
+++ test/testSampling.py
@@ -9,12 +9,33 @@ import sampling as targetCode
 @ddt
 class TestSampling(unittest.TestCase):
 
-    @data((np.array([.8,.2]), 10000,
-           [2000-2.81*math.sqrt((.9*.1)*10000), 2000+2.81*math.sqrt((.9*.1)*10000)]))
+    @data((np.array([.8, .2]), 10000, 1000,
+           [0.2 - 1.96 * math.sqrt((.8 * .2) / 1000), 0.2 + 1.96 * math.sqrt((.8 * .2) / 1000)], .95),
+          (np.array([.6, .4]), 10000, 1000,
+           [0.4 - .38 * math.sqrt((.6 * .4) / 1000), 0.4 + .38 * math.sqrt((.6 * .4) / 1000)], .3)
+          )
     @unpack
-    def test_sample_initial(self, init_dist, size, expectedResult):
-        calculatedResult = sum([sample_initial(init_dist) for i in range(size)])
-        self.assertTrue(expectedResult[0] <= calculatedResult and calculatedResult <= expectedResult[1])
+    def testSampleInitial(self, initDist, samples, size, confidenceInterval, expectedResult):
+        proportions = np.array([np.mean([sampleDistribution(initDist) for _ in range(size)]) for _ in range(samples)])
+        calculatedResult = np.mean(np.array(
+            [confidenceInterval[0] <= proportions[i] and proportions[i] <= confidenceInterval[1] for i in
+             range(samples)]))
+        print("test samp: ", calculatedResult)
+        self.assertTrue(expectedResult <= calculatedResult + 0.05 and expectedResult >= calculatedResult - 0.05)
+    
+    
+    @data((np.array([[.8, .2], [.6, .4]]), 10000, 1000,
+           [0.2 - 1.96 * math.sqrt((.8 * .2) / 1000), 0.2 + 1.96 * math.sqrt((.8 * .2) / 1000)], .95)
+          )
+    @unpack
+    def testSampleConditional(self, Distribution, samples, size, confidenceInterval, expectedResult):
+        proportions = np.array([np.mean([sampleConditional(Distribution, 0) for _ in range(size)]) for _ in range(samples)])
+        calculatedResult = np.mean(np.array(
+            [confidenceInterval[0] <= proportions[i] and proportions[i] <= confidenceInterval[1] for i in
+             range(samples)]))
+        print("test cond: ", calculatedResult)
+        self.assertTrue(expectedResult <= calculatedResult + 0.05 and expectedResult >= calculatedResult - 0.05)
+    
     def tearDown(self):
         pass
 
