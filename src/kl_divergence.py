56448315e1185e032e03019aeabe24ea4e6cc475
thomaslixu <100257462+thomaslixu@users.noreply.github.com>
2024-02-15 10:38:12 -0800

    Update kl_divergence.py

--- src/kl_divergence.py
+++ src/kl_divergence.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Calculate KL Divergence
-def KLDivergence(p, q):
+def calculateKLDivergence(p, q):
     qShift = (q + 10**-6)
     qShift = qShift/sum(qShift)
     pShift = (p + 10 ** -6)
@@ -11,5 +11,5 @@ def KLDivergence(p, q):
 # Give observation that minimizes KL Divergence
 def sythesizeObs(trueDist, condDist, obsSpace): #here the indez of dist. conditions the dist for given 0
     # Before, the dist was conditioned
-    klDivGivenObs = np.array([KLDivergence(trueDist,condDist[o]) for o in obsSpace])
+    klDivGivenObs = np.array([calculateKLDivergence(trueDist,condDist[o]) for o in obsSpace])
     return np.argmin(klDivGivenObs)
