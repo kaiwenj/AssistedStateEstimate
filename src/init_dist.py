a8406b84c2b16803486eaedb0c7ca444c417b3a6
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-02-15 11:19:57 -0800

    Update init_dist.py

--- src/init_dist.py
+++ src/init_dist.py
@@ -1,37 +1,36 @@
 import numpy as np
 
 
-def genInitDist(labels,minlength=10):
-    label_prop=np.bincount(labels,minlength=minlength)/len(labels)
-    init_dist=np.array(label_prop)
+def generateInitialDistribution(labels,minlength=10):
+    label_proportion_img=np.bincount(labels,minlength=minlength)/len(labels)
+    init_dist=np.array(label_proportion_img)
     return init_dist
 
-def genDyndDist(labels=10,actions=11):
+def generateDynamicDistribution(labels=10,actions=11):
     return np.array([np.identity(labels)]*actions)
 
-def genObsProb(dat,patterns,dataset):
-    img,label=dat
+
+def generateObservationProbability(current_img,patterns,dataset):
+    img,label=current_img
     t_img,t_label=dataset
     subImg=t_img[np.where(t_label == label)]
     numImg=len(subImg)
     patrn=img[patterns,:]
-    count=0
-    for i in range(numImg):
-        if np.array_equal(subImg[i,patterns,:],patrn):
-            count=count+1
+    count = sum([1 for i in range(numImg) if np.array_equal(subImg[i, patterns, :], patrn)])
     return count/numImg
 
+def generateInitialDistributionImg(sample_img):
+    img_len=len(sample_img)
+    img_proportion=np.ones(img_len)/img_len
+    return img_proportion
 
-def genInitDistImg(s_img):
-    img_len=len(s_img)
-    img_prop=np.ones(img_len)/img_len
-    return img_prop
+def generateDynamicDistributionImg(current_img,next_img):
+    if(np.array_equal(current_img,next_img)):
+        return 1
+    else:
+        return 0
 
-def genDynDistImg(s_img,actions=11):
-    img_len = len(s_img)
-    return np.array([np.identity(img_len)]*actions)
-
-def genObsProbImg(patterns,img_size=28):
+def generateObservationProbabilityImg(patterns,current_img):
+    img_size=current_img.shape[0]
     return len(patterns)/img_size
-
 
