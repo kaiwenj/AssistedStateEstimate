e4cf5c3ab16412e14718c8e81f76d5572bcd7e48
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-02-29 11:06:11 -0800

    Update genObsProb.py

--- src/genObsProb.py
+++ src/genObsProb.py
@@ -1,16 +1,5 @@
 import numpy as np
 
-def genObservationProbAll(patterns,dataset,state):
-    t_img, t_label = dataset
-    index = np.invert(np.isnan(patterns))
-    patrn = patterns[index]
-    subImg = t_img[np.where(t_label == state)]
-    numImg = len(subImg)
-    count = sum([1 for j in range(numImg) if np.array_equal(subImg[j,:,:][index], patrn)])
-    prob=count/numImg
-    return prob
-
-
 def genObservationProbAll(pattern, dataset, state):
     images, labels = dataset
     imagesFromState = images[np.where(labels == state)]
