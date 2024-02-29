29c3af8ccdc94ba101af0afcce031d8ace4efdc6
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-02-29 11:16:54 -0800

    Add files via upload

---
+++ src/init_dist.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def generateInitialDistribution(labels,minlength=10):
+    label_proportion_img=np.bincount(labels,minlength=minlength)/len(labels)
+    init_dist=np.array(label_proportion_img)
+    return init_dist
+
+def generateDynamicDistribution(labels=10,actions=11):
+    return np.array([np.identity(labels)]*actions)
+
+
+def generateObservationProb(current_img,pattern,dataset):
+    images, labels = dataset
+    s_image, s_label = current_img
+    imagesFromState = images[np.where(labels == s_label)]
+    prob = np.mean(np.all(np.all((pattern == imagesFromState) | (np.isnan(pattern)), axis=2), axis=1))
+    return prob
+
+
+def generateInitialDistributionImg(sample_img):
+    img_len=len(sample_img)
+    img_proportion=np.ones(img_len)/img_len
+    return img_proportion
+
+def generateDynamicDistributionImg(current_img,next_img):
+    return np.sum((np.array_equal(current_img,next_img)))
+
+def generateObservationProbImg(patterns,current_img):
+    img_size=current_img.shape
+    patrn = np.isnan(patterns)
+    return np.sum(patrn)/(img_size[0]*img_size[1])
+
