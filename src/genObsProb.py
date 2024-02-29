132e818231d6c597fc4c00de21af31b508016a16
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-02-29 10:31:20 -0800

    Update genObsProb.py

--- src/genObsProb.py
+++ src/genObsProb.py
@@ -14,7 +14,7 @@ def genObservationProbAll(patterns,dataset,state):
 def genObservationProbAll(pattern, dataset, state):
     images, labels = dataset
     imagesFromState = images[np.where(labels == state)]
-    prob = np.mean(np.all(np.all((pattern == imagesFromState) | (np.isnan(pattern)), axis=2), axis = 1)))
+    prob = np.mean(np.all(np.all((pattern == imagesFromState) | (np.isnan(pattern)), axis=2), axis = 1))
     return prob
 
 
