401cca4cb83322200ac7e397bc5bbc48505f8a5c
WilliamPan02 <103080140+WilliamPan02@users.noreply.github.com>
2024-05-22 16:24:40 -0700

    Update ImgEnumerate.py

--- src/ImgEnumerate.py
+++ src/ImgEnumerate.py
@@ -11,24 +11,3 @@ def ImgEnumerate(OriginalImg,KImg):
     return OutImg
 
 
-c = np.array([[1, 2, 1],
-                  [2, 2, 2],
-                  [7, 9, 3]], dtype=np.float32)
-
-c[1,:]=np.nan
-
-
-d=np.array([[1, 2, 1],
-                  [2, 2, 2],
-                  [7, 9, 3]], dtype=np.float32)
-
-
-ImgEnumerate(d,c)
-
-np.where(np.all(np.isnan(c),axis=1))[0][1]
-
-np.array(list(range(0,3)))[np.all(np.isnan(c),axis=1)]
-
-
-for i in range(4):
-    print(i)
